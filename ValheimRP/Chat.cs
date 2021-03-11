1d2a56de01ff72e9a9c34e84eb3059b58b04ab6e
ryan broman <ryanjbroman@gmail.com>
2021-03-10 21:52:22 -0800

    Add OOC chat

--- ValheimRP/Chat.cs
+++ ValheimRP/Chat.cs
@@ -2,24 +2,54 @@
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
+// ReSharper disable StringIndexOfIsCultureSpecific.1
 
 namespace ValheimRP {
-    [HarmonyPatch(typeof(Chat), "OnNewChatMessage")]
+    [UsedImplicitly]
+    [HarmonyPatch(typeof(Chat))]
     internal class VRPChat {
         /// <summary>
         /// Logs all chat messages and pings.
         /// </summary>
+        /// <param name="__instance">Chat instance.</param>
         /// <param name="senderID">ID of the player sending that chat.</param>
         /// <param name="pos">Sender's current position, or ping position if ping.</param>
         /// <param name="type">Channel the chat is being sent through.</param>
         /// <param name="user">Sender's character name.</param>
         /// <param name="text">Content of the chat.</param>
-        /// <returns>true always</returns>
-        [UsedImplicitly]
-        private static bool Prefix(ref long senderID, ref Vector3 pos, ref Talker.Type type,
+        [HarmonyPatch("OnNewChatMessage")]
+        private static bool Prefix(Chat __instance, ref long senderID, ref Vector3 pos, ref Talker.Type type,
             ref string user, ref string text) {
             Log.LogInfo($"[Chat] [{Enum.GetName(typeof(Talker.Type), type)}] {user} ({senderID}) {pos}: {text}");
-            return true;
+            
+            // Prevent OOC messages from appearing on the player's head.
+            if(!IsOOC(text)) return true;
+            text = text.Replace('<', ' ');
+            text = text.Replace('>', ' ');
+            __instance.AddString(user, text, type);
+            return false;
+        }
+
+        /// <summary>
+        /// Directs custom chats to pseudo-channels.
+        /// </summary>
+        /// <param name="__instance">Chat instance</param>
+        /// <param name="user">Sender's character name</param>
+        /// <param name="text">Actual content of the message</param>
+        /// <param name="type">Channel the chat is being sent through.</param>
+        [HarmonyPatch("AddString", typeof(string), typeof(string), typeof(Talker.Type))]
+        private static bool Prefix(Chat __instance, ref string user, ref string text, ref Talker.Type type) {
+            if (!IsOOC(text)) return true;
+            text = text.Substring(text.IndexOf(" "));
+
+            var message = $"<color={ColorUtility.ToHtmlStringRGBA(Color.blue)}>[OOC] {user}:{text}</color>";
+            __instance.AddString(message);
+            
+            return false;
+        }
+
+        private static bool IsOOC(string input) {
+            return input.ToLower().StartsWith("/ooc") || input.ToLower().StartsWith("//");
         }
     }
 }
